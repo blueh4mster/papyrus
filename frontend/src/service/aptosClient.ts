2c5503545619f1970575f0828c9639a3c92adef5
blueh4mster <https://blueh4mster@github.com>
2025-07-27 12:14:44 +0530

    added claim from aptos function

--- frontend/src/service/aptosClient.ts
+++ frontend/src/service/aptosClient.ts
@@ -7,26 +7,30 @@ const resolverAccount = Account.fromPrivateKey({
   privateKey: new Ed25519PrivateKey(process.env.NEXT_PUBLIC_APTOS_RESOLVER_KEY!),
 });
 
+const strToBytes = (str: string) =>
+  Array.from(new TextEncoder().encode(str));
+
 export async function redeemOnAptos({
-  hashlock,
-  receiver,
-  amount,
-  expiry,
+  sender,
+  id,
+  secret
 }: {
-  hashlock: string;
-  receiver: string;
-  amount: number;
-  expiry: number;
+  sender: string;
+  id: string;
+  secret: string;
 }) {
+  const arg1 = strToBytes(sender);
+  const arg2 = strToBytes(id);
+  const arg3 = strToBytes(secret);
+
   const tx = await aptos.transaction.build.simple({
     sender: resolverAccount.accountAddress,
     data: {
       function: "0x55e3708368f22c77c445935373cc68c187d2171a395177fd79a25b185fc38461::htlc_aptos::claim",
       functionArguments: [
-        `0x${hashlock}`,
-        receiver,
-        amount,
-        expiry,
+        arg1,
+        arg2,
+        arg3
       ],
     },
   });
